bb31232dcb56645c825a989d4b7871d22ff62c14
Artur Mushulov <mushulov.a@firstmk.ru>
2024-06-15 10:58:50 +0300

    add geozone

--- src/app/components/main/components/map/map.component.ts
+++ src/app/components/main/components/map/map.component.ts
@@ -18,7 +18,7 @@ export class MapComponent implements AfterViewInit {
   private initMap(): void {
     this.map = L.map('map', {
       center: [45.035804, 39.018066],
-      zoom: 12,
+      zoom: 18,
       zoomControl: false
     });
 
@@ -29,11 +29,53 @@ export class MapComponent implements AfterViewInit {
     });
 
     const myIcon = L.icon({
-      iconUrl: 'assets/icons/marker.png',
+      iconUrl: 'assets/icons/marker.svg',
+    })
+    const myIcon1 = L.icon({
+      iconUrl: 'assets/icons/marker-task-out.svg',
+    })
+    const myIcon2 = L.icon({
+      iconUrl: 'assets/icons/marker-task-over.svg',
+    })
+    const myIcon3 = L.icon({
+      iconUrl: 'assets/icons/marker-task-progress.svg',
     })
 
     tiles.addTo(this.map);
-    L.marker([45.035804, 39.018066], {icon: myIcon})
+    L.marker([45.035804, 39.017066], {icon: myIcon})
       .addTo(this.map)
+    L.marker([45.035905, 39.016066], {icon: myIcon1})
+      .bindPopup('Уборка территории')
+      .addTo(this.map)
+    L.marker([45.035906, 39.018066], {icon: myIcon2})
+      .bindPopup('Заливка фундамента')
+      .addTo(this.map)
+    L.marker([45.035608, 39.017066], {icon: myIcon3})
+      .bindPopup('Ограждение территории')
+      .addTo(this.map)
+
+    const circle = L.circle([45.035915, 39.016176], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 10
+    }).addTo(this.map);
+
+    const polygon = L.polygon([
+      [45.036906, 39.018066],
+      [45.035906, 39.018576],
+      [45.035806, 39.018166],
+      [45.036006, 39.017066]
+    ]).addTo(this.map);
+
+    const circle1 = L.circle([45.035608, 39.017066], {
+      color: '#2ba266',
+      fillColor: '#2ba266',
+      fillOpacity: 0.5,
+      radius: 10
+    }).addTo(this.map);
+
   }
+
+
 }
